efca2bc4acaebbe339660fdf68ca8ac50ba87dd1
junpei5060 <junpei.nakazawa.5060@gmail.com>
2021-01-23 22:49:36 +0900

    deploy

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@
   width: 100%;
   height: auto;
   z-index: -100;
-  background: url('image/dark.png') no-repeat;
+  background: url('/image/dark.png') no-repeat;
   background-size: cover;
   background-position: center center;
 }
